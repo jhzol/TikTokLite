6396957c750aeed166674e3e6ceb1ac4e4ab3362
“jhzol” <253095504@qq.com>
2022-05-17 16:11:29 +0800

    更新sql脚本

--- TikTokLite.sql
+++ TikTokLite.sql
@@ -11,17 +11,17 @@
  Target Server Version : 80029
  File Encoding         : 65001
 
- Date: 14/05/2022 16:33:43
+ Date: 17/05/2022 15:33:29
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for Comment
+-- Table structure for comments
 -- ----------------------------
-DROP TABLE IF EXISTS `Comment`;
-CREATE TABLE `Comment`  (
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE `comments`  (
   `comment_id` bigint NOT NULL AUTO_INCREMENT,
   `user_id` bigint NOT NULL,
   `video_id` bigint NOT NULL,
@@ -30,45 +30,45 @@ CREATE TABLE `Comment`  (
   PRIMARY KEY (`comment_id`) USING BTREE,
   INDEX `commentUser`(`user_id`) USING BTREE,
   INDEX `commentVideo`(`video_id`) USING BTREE,
-  CONSTRAINT `commentuser` FOREIGN KEY (`user_id`) REFERENCES `User` (`user_id`) ON DELETE CASCADE ON UPDATE RESTRICT,
-  CONSTRAINT `commentvideo` FOREIGN KEY (`video_id`) REFERENCES `Video` (`video_id`) ON DELETE CASCADE ON UPDATE RESTRICT
+  CONSTRAINT `commentuser` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  CONSTRAINT `commentvideo` FOREIGN KEY (`video_id`) REFERENCES `videos` (`video_id`) ON DELETE CASCADE ON UPDATE RESTRICT
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
--- Table structure for Favorite
+-- Table structure for favorites
 -- ----------------------------
-DROP TABLE IF EXISTS `Favorite`;
-CREATE TABLE `Favorite`  (
+DROP TABLE IF EXISTS `favorites`;
+CREATE TABLE `favorites`  (
   `favorite_id` bigint NOT NULL AUTO_INCREMENT,
   `user_id` bigint NOT NULL,
   `video_id` bigint NOT NULL,
   PRIMARY KEY (`favorite_id`) USING BTREE,
   INDEX `favoriteUser`(`user_id`) USING BTREE,
   INDEX `favoriteVideo`(`video_id`) USING BTREE,
-  CONSTRAINT `favoriteuser` FOREIGN KEY (`user_id`) REFERENCES `User` (`user_id`) ON DELETE CASCADE ON UPDATE RESTRICT,
-  CONSTRAINT `favoritevideo` FOREIGN KEY (`video_id`) REFERENCES `Video` (`video_id`) ON DELETE CASCADE ON UPDATE RESTRICT
+  CONSTRAINT `favoriteuser` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  CONSTRAINT `favoritevideo` FOREIGN KEY (`video_id`) REFERENCES `videos` (`video_id`) ON DELETE CASCADE ON UPDATE RESTRICT
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
--- Table structure for Relation
+-- Table structure for relations
 -- ----------------------------
-DROP TABLE IF EXISTS `Relation`;
-CREATE TABLE `Relation`  (
+DROP TABLE IF EXISTS `relations`;
+CREATE TABLE `relations`  (
   `relation_id` bigint NOT NULL AUTO_INCREMENT,
   `follow_id` bigint NOT NULL,
   `follower_id` bigint NOT NULL,
   PRIMARY KEY (`relation_id`) USING BTREE,
   INDEX `FollowId`(`follow_id`) USING BTREE,
   INDEX `FollowerId`(`follower_id`) USING BTREE,
-  CONSTRAINT `followerid` FOREIGN KEY (`follower_id`) REFERENCES `User` (`user_id`) ON DELETE CASCADE ON UPDATE RESTRICT,
-  CONSTRAINT `followid` FOREIGN KEY (`follow_id`) REFERENCES `User` (`user_id`) ON DELETE CASCADE ON UPDATE RESTRICT
+  CONSTRAINT `followerid` FOREIGN KEY (`follower_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  CONSTRAINT `followid` FOREIGN KEY (`follow_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE RESTRICT
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
--- Table structure for User
+-- Table structure for users
 -- ----------------------------
-DROP TABLE IF EXISTS `User`;
-CREATE TABLE `User`  (
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
   `user_id` bigint NOT NULL AUTO_INCREMENT,
   `user_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
@@ -76,23 +76,23 @@ CREATE TABLE `User`  (
   `follower_count` bigint NULL DEFAULT NULL,
   `token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   PRIMARY KEY (`user_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
--- Table structure for Video
+-- Table structure for videos
 -- ----------------------------
-DROP TABLE IF EXISTS `Video`;
-CREATE TABLE `Video`  (
+DROP TABLE IF EXISTS `videos`;
+CREATE TABLE `videos`  (
   `video_id` bigint NOT NULL AUTO_INCREMENT,
   `author_id` bigint NOT NULL,
   `play_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `cover_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `favorite_count` bigint NULL DEFAULT NULL,
   `comment_count` bigint NULL DEFAULT NULL,
-  `publish_time` datetime NULL DEFAULT NULL,
+  `publish_time` bigint NULL DEFAULT NULL,
   PRIMARY KEY (`video_id`) USING BTREE,
   INDEX `user`(`author_id`) USING BTREE,
-  CONSTRAINT `authorid` FOREIGN KEY (`author_id`) REFERENCES `User` (`user_id`) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+  CONSTRAINT `authorid` FOREIGN KEY (`author_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 SET FOREIGN_KEY_CHECKS = 1;
